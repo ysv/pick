2c9c76cd5d227606c2fa1e3ec9c5bac383e2e55e
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-03-22 01:09:10 +0200

    Add before and after hooks to cli. Rename app to cliApp

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,33 @@
 package cmd
 
 import (
+	"github.com/ysv/pick/app"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
 func Run(version string) error {
-	app := cli.NewApp()
+	cliApp := cli.NewApp()
 
-	app.Name = "Pick"
-	app.Description = "lightning and reliable website analytics"
-	app.Version = version
+	cliApp.Name = "Pick"
+	cliApp.Description = "lightning and reliable website analytics"
+	cliApp.Version = version
 
-	app.Commands = []cli.Command{
+	cliApp.Before = beforeRun
+	cliApp.After = afterRun
+
+	cliApp.Commands = []cli.Command{
 		serverCommand,
 		databaseCommand,
 	}
 
-	app.Run(os.Args)
+	cliApp.Run(os.Args)
 	return nil
 }
 
 func beforeRun(c *cli.Context) error {
+	app.InitApp()
 	return nil
 }
 
