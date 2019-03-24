caedfd52140062316f225ff162ea924665cd7b5a
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-03-24 13:53:03 +0200

    Remove debug messages from config.go & minor cleanup

--- app/config.go
+++ app/config.go
@@ -5,29 +5,19 @@ import (
 	"github.com/spf13/viper"
 )
 
-//type Config struct {
-//	*viper.Viper
-//}
-
 func initConfig(){
 	config := viper.New()
 
 	config.SetConfigType("yml")
 	config.AddConfigPath(".")
+
+	// TODO: Use default config unless file exists.
 	config.SetConfigFile("config.yml")
-	config.SetConfigFile("config.yaml")
 
 	err := config.ReadInConfig()
-	// TODO: Use logrus panic.
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-
-	// Check if we need this feature.
-	config.WatchConfig()
-
-	fmt.Println(config.AllKeys())
-	config.Debug()
 
 	app.config = config
 }
